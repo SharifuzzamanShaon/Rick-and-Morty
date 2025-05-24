939167b57341c67af985a4e3ce5d5248b53b8ffa
sharifuzzamanshaon <shasan2200@gmail.com>
2025-05-24 19:47:27 +0600

    Add layout and homepage components; remove old page.js

---
+++ src/app/(site)/page.js
@@ -0,0 +1,11 @@
+import Homepage from "./(pages)/HomePage/Homepage";
+
+export default function Home() {
+  return (
+    <>
+      <div className="">
+        <Homepage/>
+      </div>
+    </>
+  );
+}
