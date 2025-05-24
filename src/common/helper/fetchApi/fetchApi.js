69ffc3956e5129600ceafef21b2f59f5aa1b5036
sharifuzzamanshaon <shasan2200@gmail.com>
2025-05-24 19:48:13 +0600

    Add fetchApi helper with methods to fetch characters, episodes, and locations

---
+++ src/common/helper/fetchApi/fetchApi.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const HttpKit={
+fetchAllCast:async()=>{
+        try {
+            const response = await axios.get(`${BASE_URL}/character`);
+            const data = await response.data;
+            return data.results;
+        } catch (error) {
+            console.log(error);
+        }
+    },
+fetchAllEpisode:async()=>{
+    try {
+        const response =await axios.get(`${BASE_URL}/episode`);
+        const data = await response.data;
+        return data.results;
+    } catch (error) {
+        console.log(error);
+    }
+},
+fetchAllLocation:async()=>{
+    try {
+        const response =await axios.get(`${BASE_URL}/location`);
+        const data = await response.data;
+        return data.results;
+    } catch (error) {
+        console.log(error);
+    }
+}
+}
+export default HttpKit
