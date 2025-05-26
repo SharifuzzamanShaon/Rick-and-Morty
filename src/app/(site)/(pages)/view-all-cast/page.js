90ec5c1dca7b5b6ce11732525ad5305377feddc2
sharifuzzamanshaon <shasan2200@gmail.com>
2025-05-26 23:20:48 +0600

    Add loading spinner for cast data

--- src/app/(site)/(pages)/view-all-cast/page.js
+++ src/app/(site)/(pages)/view-all-cast/page.js
@@ -33,29 +33,35 @@ const ViewAllCast = () => {
             The Cast
           </h2>
           <div className="grid grid-cols-2 sm:grid-cols-3  md:grid-cols-4 lg:grid-cols-5 gap-6">
-            {cast.map((character, idx) => (
-              <div
-                key={idx}
-                className="relative p-[.4px] custom-clip bg-gradient-to-t from-[#7ff633] to-[#1cdbda] cursor-pointer rounded-[8px]"
-                onClick={() => router.push(`/cast-details/${character.id}`)}
-              >
-                <div className="p-2 pb-2 bg-[#1e293b] text-white overflow-hidden custom-clip w-full h-full relative rounded-[8px]">
-                  <div className="absolute inset-0 bg-[#1e293b] opacity-20 pointer-events-none custom-clip z-0"></div>
-                  <div className="relative z-10 rounded-[8px]">
-                    <Image
-                      src={character.image}
-                      alt={character.name}
-                      width={150}
-                      height={150}
-                      className="rounded-[8px] w-full h-[150px] object-cover hover:scale-105 transition-all duration-300"
-                    />
-                    <p className="mt-2 text-left text-sm font-medium py-2 rounded-[8px]">
-                      {character.name}
-                    </p>
+            {cast.length === 0 ? (
+              <div className="fixed inset-0 flex items-center justify-center z-50 bg-transparent">
+                <div className="w-10 h-10 border-4 border-[#9efe01] border-t-transparent rounded-full animate-spin"></div>
+              </div>
+            ) : (
+              cast.map((character, idx) => (
+                <div
+                  key={idx}
+                  className="relative p-[.4px] custom-clip bg-gradient-to-t from-[#7ff633] to-[#1cdbda] cursor-pointer rounded-[8px] "
+                  onClick={() => router.push(`/cast-details/${character.id}`)}
+                >
+                  <div className="p-2 pb-2 bg-[#1e293b] text-white overflow-hidden custom-clip w-full h-full relative rounded-[8px]">
+                    <div className="absolute inset-0 bg-[#1e293b] opacity-20 pointer-events-none custom-clip z-0"></div>
+                    <div className="relative z-10 rounded-[8px]">
+                      <Image
+                        src={character.image}
+                        alt={character.name}
+                        width={150}
+                        height={150}
+                        className="rounded-[8px] w-full h-[150px] object-cover hover:scale-105 transition-all duration-300"
+                      />
+                      <p className="mt-2 text-left text-sm font-medium py-2 rounded-[8px]">
+                        {character.name}
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))
+            )}
           </div>
         </section>
       </div>
