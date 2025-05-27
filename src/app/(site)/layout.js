c7ea037e1b8617dfb98094213543336483c4925e
sharifuzzamanshaon <shasan2200@gmail.com>
2025-05-27 16:07:10 +0600

    Refactor layout component by removing unnecessary body class and organizing imports

--- src/app/(site)/layout.js
+++ src/app/(site)/layout.js
@@ -1,10 +1,10 @@
 import "../globals.css";
+import { ttTravelsLight } from "@/common/helper/font/font";
 
 export const metadata = {
   title: "The Rick & Morty Wiki",
   description: "The Rick & Morty Wiki",
 };
-
 
 export default function RootLayout({ children }) {
   return (
@@ -14,9 +14,7 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="The Rick & Morty Wiki" />
       </head>
-      <body 
-        className={``}
-      >
+      <body>
         {children}
       </body>
     </html>
