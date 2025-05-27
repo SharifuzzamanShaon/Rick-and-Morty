3ca443bfa5f1ac3722b1b3cde6733a12f6f29f23
sharifuzzamanshaon <shasan2200@gmail.com>
2025-05-27 16:08:19 +0600

    Add local font definitions for TTTravels family to enhance typography

---
+++ src/common/helper/font/font.js
@@ -0,0 +1,66 @@
+import { Encode_Sans_SC as encodeSansSC, Orbitron } from 'next/font/google';
+
+import localFont from 'next/font/local'
+
+//  export const tttravelsBlack = localFont({
+  
+//   src: './../../../../fonts/TTTravels-Black.ttf',
+//   variable: "--font-tttravels-black",
+//   weight: "900",
+
+// })
+// app/fonts.js
+
+export const ttTravelsBlack = localFont({
+  src: './../../../../fonts/TTTravels-Black.ttf',
+  weight: '1200',
+  variable: 'tttravels-black',
+});
+
+export const ttTravelsBlackItalic = localFont({
+  src: './../../../../fonts/TTTravels-BlackItalic.ttf',
+  weight: '900',
+  variable: 'tttravels-black-italic',
+});
+
+export const ttTravelsBold = localFont({
+  src: './../../../../fonts/TTTravels-Bold.ttf',
+  weight: '700',
+  variable: 'tttravels-bold',
+});
+
+export const ttTravelsRegular = localFont({
+  src: './../../../../fonts/TTTravels-Regular.ttf',
+  weight: '200',
+  variable: 'tttravels-regular',
+});
+
+export const ttTravelsLight = localFont({
+  src: './../../../../fonts/TTTravels-Light.ttf',
+  weight: '200',
+  variable: 'tttravels-light',
+});
+export const ttTravelsLightItalic = localFont({
+  src: './../../../../fonts/TTTravels-LightItalic.ttf',
+  weight: '200',
+  variable: 'tttravels-light-italic',
+}); 
+export const ttTravelsMedium = localFont({
+  src: './../../../../fonts/TTTravels-Medium.ttf',
+  weight: '500',
+  variable: 'tttravels-medium',
+});
+  
+export const ttTravelsMediumItalic = localFont({
+  src: './../../../../fonts/TTTravels-MediumItalic.ttf',
+  weight: '500',
+  variable: 'tttravels-medium-italic',
+});
+
+
+export const ttTravelsThin = localFont({
+  src: './../../../../fonts/TTTravels-Thin.ttf',
+  weight: '100',
+  variable: 'tttravels-thin',
+});
+  
